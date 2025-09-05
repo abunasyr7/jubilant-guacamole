af2baa0d4f658cc3e86bd365ef405c862732e324
Abunasyr <abunasyr1@gmail.com>
2025-09-05 22:20:11 +0500

    feat: добавил отлов ошибок формаы

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import "./styles/main.sass";
 import { initPopup } from "./components/popup";
+import { setupEmailAndPasswordValidation } from "./components/form";
 
 document.addEventListener("DOMContentLoaded", () => {
   initPopup();
+  setupEmailAndPasswordValidation();
 });
